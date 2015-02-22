c32e0e9c02e3724197c015f0966d97de4730c1a0
Bryn Dole <bryn.dole@gmail.com>
2015-02-21 22:37:18 -0600

    added more test problems. Easier data entry method

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -108,34 +108,66 @@ def solve_sudoku(game):
             break
         backtrack(game,[[coords[0], coords[1]]])
 
+def move_list_from_strings(lines):
+    moves = []
+    for row,line in enumerate(lines):
+        for col, val in enumerate(line.split()):
+            if val != '.':
+                moves.append([row,col,int(val)])
+    return moves
 
 #
 # Set up Game board
 #
 game = {"filled": 0, "board": empty_board(9), "solved": False, "tries": 0 }
 
-# From The Algorithm Design Manual 2nd Edition (S. S. Skiena) Page 239
-# this is a "hard" problem
-make_moves(game,[[0,7,1],[0,8,2],[1,4,3],[1,5,5],[2,3,6],[2,7,7],[3,0,7],[3,6,3],[4,3,4],[4,6,8],[5,0,1],[6,3,1],[6,4,2],[7,1,8],[7,7,4],[8,1,5],[8,6,6]])
+# choose problem 1-4
+game_number = 4
 
-# Some Hints (to make it faster for testing)
-make_moves(game,[[0,0,6],[0,1,7],[0,2,3],[1,0,9],[2,0,8]])   # about 0.11 s
-# make_moves(game,[[0,0,6],[0,1,7]])                           # about 0.25 s
-# make_moves(game,[[0,0,6]])                                   # about 10 s
-# with no hints it takes about 20 s
+if game_number == 1:
+    # From The Algorithm Design Manual 2nd Edition (S. S. Skiena) Page 239
+    # this is a "hard" problem
+    make_moves(game,[[0,7,1],[0,8,2],[1,4,3],[1,5,5],[2,3,6],[2,7,7],[3,0,7],[3,6,3],[4,3,4],[4,6,8],[5,0,1],[6,3,1],[6,4,2],[7,1,8],[7,7,4],[8,1,5],[8,6,6]])
+    
+    # Some Hints (to make it faster for testing)
+    make_moves(game,[[0,0,6],[0,1,7],[0,2,3],[1,0,9],[2,0,8]])   # about 0.11 s
+    # make_moves(game,[[0,0,6],[0,1,7]])                           # about 0.25 s
+    # make_moves(game,[[0,0,6]])                                   # about 10 s
+    # with no hints it takes about 20 s
 
-# solution for reference
-solution = ["6 7 3 8 9 4 5 1 2", 
-            "9 1 2 7 3 5 4 8 6",
-            "8 4 5 6 1 2 9 7 3",
-            "7 9 8 2 6 1 3 5 4",
-            "5 2 6 4 7 3 8 9 1",
-            "1 3 4 5 8 9 2 6 7",
-            "4 6 9 1 2 8 7 3 5",
-            "2 8 7 3 5 6 1 4 9",
-            "3 5 1 9 4 7 6 2 8"]
+    # solution for reference
+    solution = ["6 7 3 8 9 4 5 1 2", 
+                "9 1 2 7 3 5 4 8 6",
+                "8 4 5 6 1 2 9 7 3",
+                "7 9 8 2 6 1 3 5 4",
+                "5 2 6 4 7 3 8 9 1",
+                "1 3 4 5 8 9 2 6 7",
+                "4 6 9 1 2 8 7 3 5",
+                "2 8 7 3 5 6 1 4 9",
+                "3 5 1 9 4 7 6 2 8"]
+elif game_number == 2:
+    # Easy problem from American Airlines inflight Magazine
+    make_moves(game,[[0,2,7],[0,4,6],[1,0,8],[1,2,3],[1,6,1],[1,7,4],[1,8,2],[2,0,9],[2,1,2],[2,3,8],[2,6,5],[3,1,5],[3,3,2],[3,6,9],[3,8,4],[4,0,2],[4,3,5],[4,4,8],[4,5,4],[4,8,1],[5,0,3],[5,2,4],[5,5,1],[5,7,2],[6,2,5],[6,5,6],[6,7,1],[6,8,8],[7,0,7],[7,1,3],[7,2,2],[7,6,6],[7,8,9],[8,4,2],[8,6,4]])
+elif game_number == 3:
+    # medium problem from American Airlines inflight Magazine
+    make_moves(game,[[0,2,5],[0,3,3],[0,6,1],[0,8,4],[1,0,9],[1,1,3],[1,3,5],[1,6,2],[2,5,6],[2,7,5],[3,1,5],[3,4,2],[3,5,8],[3,8,1],[4,1,9],[4,2,4],[4,6,8],[4,7,2],[5,0,8],[5,3,6],[5,4,1],[5,7,7],[6,1,6],[6,3,1],[7,2,2],[7,5,5],[7,7,1],[7,8,8],[8,0,5],[8,2,9],[8,5,2],[8,6,6]])
+else:
+    # hard problem from American Airlines inflight Magazine
+    problem4 = ["3 7 . 4 . . 1 . .",
+                ". . . . . . . 2 .",
+                ". . . 2 . 1 . 5 3",
+                ". . . . . 9 6 1 4",
+                "4 . . . 2 . . . 7",
+                "7 6 9 3 . . . . .",
+                "9 2 . 1 . 6 . . .",
+                ". 1 . . . . . . .",
+                ". . 8 . . 2 . 9 1"]
+    make_moves(game,move_list_from_strings(problem4))
 
-print("--- starting possition ---")
+#
+# Solve the puzzle
+#
+print("--- starting possition", game_number, "---")
 print_game(game)
 solve_sudoku(game)
 print("--- final possition ---")
