883afe75bc1bc67c301bc0709577fec6a49b0581
randomstring <bryn.dole@gmail.com>
2015-12-16 09:40:16 -0800

    add connect four scripts.

---
+++ c4_cvc.py
@@ -0,0 +1,5 @@
+#!/Library/Frameworks/Python.framework/Versions/3.5/bin/python3
+
+import connect_four
+
+connect_four.computer_vs_computer()
---
+++ c4_tests.py
@@ -0,0 +1,6 @@
+#!/Library/Frameworks/Python.framework/Versions/3.5/bin/python3
+
+import connect_four as c4
+
+c4.tests()
+
