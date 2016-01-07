d6768ce8c4eba92a0dcc4bfd146bf3e56d7aaa9a
randomstring <bryn.dole@gmail.com>
2016-01-06 21:40:19 -0800

    checkpoint. working on reading puzzle file and benchmark mode

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -2,6 +2,7 @@
 
 import sudoku
 import argparse
+import time
 
 # parse arguments
 parser = argparse.ArgumentParser(description='Solve a Sudoku puzzle.',
@@ -14,12 +15,17 @@ Example puzzle format:
 group = parser.add_mutually_exclusive_group()
 group.add_argument('-p', metavar='puzzle', help='sudoku puzzle string')
 group.add_argument('-f', metavar='filename', help='filename containing puzzle string(s)')
-
+parser.add_argument('-b','--benchmark', action='store_true', help='time total amount of time for all puzzles')
+parser.add_argument('-v','--verbose', action='store_true', help='more verbose output')
 args = parser.parse_args()
+
+start_time = time.time()
+count = 0
 
 # load game
 puzzle_string = args.p
-print(puzzle_string)
+if args.verbose:
+    print(puzzle_string)
 
 game = sudoku.new_game(puzzle_string)
                   
@@ -28,3 +34,7 @@ sudoku.solve(game)
 
 # print result
 sudoku.print_game(game)
+
+elapsed = time.time() - start_time
+if args.verbose or args.benchmark:
+    print('{0:.3f} seconds elapsed time for {1} puzzles in file {2}'.format(elapsed,count,''))
