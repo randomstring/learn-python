6ecd3c6aefca53a3f9f8e8448c96db5bbb18af7f
Bryn Dole <bryn.dole@gmail.com>
2017-12-13 12:27:32 -0800

    python style update

--- max_overlap_score.py
+++ max_overlap_score.py
@@ -7,12 +7,16 @@
 
 from random import randint
 
+
 def gen_random_jobs(jobs, time_range, cost_range):
     """
     input: the number of jobs (int), a time range(int), cost range (int)
     output: list of tuples (start_time, duration, cost)
     """
-    return [ [s, s+d if s+d <= time_range else time_range + 1 , c  ] for s,d,c in [ ( randint(0,time_range), randint(1,time_range), randint(1,cost_range) ) for i in range(jobs) ]]
+    return [[s, s+d if s+d <= time_range else time_range + 1, c]
+            for s, d, c in [(randint(0, time_range), randint(1, time_range),
+                             randint(1, cost_range)) for i in range(jobs)]]
+
 
 job_list = gen_random_jobs(10, 40, 100)
 
@@ -20,8 +24,9 @@ job_list = gen_random_jobs(10, 40, 100)
 # Sorting should not effect the result, TODO: demonstrate this
 #
 print("unsorted:", job_list)
-job_list.sort(key= lambda x:x[0])
+job_list.sort(key=lambda x: x[0])
 print("sorted:", job_list)
+
 
 def cost(job_list):
     """
@@ -33,20 +38,21 @@ def cost(job_list):
         cost += job[2]
     return cost
 
+
 def jobs_after(time,job_list):
     """
     input: a time and a job list
     output: return all the jobs that start after the given time
     """
-    return [job for job in job_list if (job[0] > time) ]
+    return [job for job in job_list if (job[0] > time)]
 
 def find_best_jobs(job_list):
     """
-    input: job list, sorted by start time 
+    input: job list, sorted by start time
     output: return tuple with best cost and the list of non-overlapping jobs
     """
     if (len(job_list) < 1):
-        return [0,[]]
+        return [0, []]
     # choose first job
     first_job = job_list[0]
     [a_cost, a_jobs] = find_best_jobs(jobs_after(first_job[1], job_list))
@@ -54,10 +60,11 @@ def find_best_jobs(job_list):
     a_jobs.insert(0, first_job)
     [b_cost, b_jobs] = find_best_jobs(job_list[1:])
     if (b_cost > a_cost):
-        #print("best (skip first):", b_cost, b_jobs)
-        return [ b_cost, b_jobs ]
-    #print("best (with first):", a_cost, a_jobs)
-    #print("   first:", first_job)
-    return [a_cost, a_jobs ]
+        # print("best (skip first):", b_cost, b_jobs)
+        return [b_cost, b_jobs]
+    # print("best (with first):", a_cost, a_jobs)
+    # print("   first:", first_job)
+    return [a_cost, a_jobs]
+
 
 print("best jobs:", find_best_jobs(job_list))
